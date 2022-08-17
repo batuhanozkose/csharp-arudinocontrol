972463f9c4b763d4e8df72df1bbeae7ac9bd4c42
Batuhan Özköse <batuhan.ozkose@outlook.com>
2022-08-17 12:56:32 +0300

    Update main.ino

--- C# - Arduino - Control/Arduino Codes/main/main.ino
+++ C# - Arduino - Control/Arduino Codes/main/main.ino
@@ -1,31 +1,22 @@
 void setup() {
   pinMode(8, OUTPUT);
-   //discordtr.org
   Serial.begin(9600);
-   //discordtr.org
 }
 
- //discordtr.org
 void loop()
 {
-   //discordtr.org
   if(Serial.available())
-   //discordtr.org
   {
     char c = Serial.read();
     if(c == '1')
     {
       digitalWrite(8,HIGH);
-       //discordtr.org
       Serial.println("Led Yandı");
     }
-    else if(c == '0')  //discordtr.org
+    else if(c == '0')
     {
       digitalWrite(8,LOW);
-       //discordtr.org
       Serial.println("Led Söndü");
-       //discordtr.org
     }
   }
-   //discordtr.org
 }
